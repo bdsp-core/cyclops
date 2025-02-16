00de5cb98ca2fb4b91398bebdfca82eeea33c183
Moritz Alkofer <moritz@Moritzs-MacBook-Air.local>
2025-02-16 11:47:54 +0100

    initial commit

--- local_utils/models/spikenet.py
+++ local_utils/models/spikenet.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 
 class FirstConvBlock(nn.Module):
-    def __init__(self,in_channels,out_channels):
+    def __init__(self,in_channels,out_channels,dropout):
         # input shape [none, channels, ts, eeg_channels]
         # output shape 
         super().__init__()
@@ -12,7 +12,7 @@ class FirstConvBlock(nn.Module):
         self.batch_norm1 = nn.BatchNorm2d(32)
         self.leaky_relu1 = nn.LeakyReLU(0.3)
         self.max_pooling1 = nn.MaxPool2d(kernel_size=(4, 1), stride=(4, 1))
-        self.dropout1 = nn.Dropout(0.5)
+        self.dropout1 = nn.Dropout(dropout)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -25,13 +25,13 @@ class FirstConvBlock(nn.Module):
         return x
 
 class SecondConvBlock(nn.Module):
-    def __init__(self,in_channels,out_channels,n_eeg_channels):
+    def __init__(self,in_channels,out_channels,n_eeg_channels,dropout):
         super().__init__()
         self.conv3 = nn.Conv2d(in_channels, out_channels, kernel_size=(1, n_eeg_channels), stride=(1, 1), padding=0, bias=False)  # 'valid' padding in PyTorch is equivalent to padding=0
         self.conv4 = nn.Conv2d(out_channels, out_channels, kernel_size=(9, 1), stride=(1, 1), padding=(4, 0), bias=False)  # Manual padding for 'same'
         self.batch_norm2 = nn.BatchNorm2d(64)
         self.leaky_relu2 = nn.LeakyReLU(0.3)
-        self.dropout2 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(dropout)
 
     def forward(self, x):
         x = self.conv3(x)
@@ -42,7 +42,7 @@ class SecondConvBlock(nn.Module):
         return x
     
 class OtherConvBlock(nn.Module):
-    def __init__(self,in_channels,out_channels,downsample=False):
+    def __init__(self,in_channels,out_channels,dropout,downsample=False):
         super().__init__()
         self.downsample = downsample
         self.conv = nn.Conv2d(in_channels,out_channels, kernel_size=(9,1), stride=(1, 1), padding=(4,0), bias=False)  # 'valid' padding in PyTorch is equivalent to padding=0
@@ -50,7 +50,7 @@ class OtherConvBlock(nn.Module):
         self.leaky_relu = nn.LeakyReLU(0.3)
         if downsample:
             self.max_pooling = nn.MaxPool2d(kernel_size=(4, 1), stride=(4, 1))
-        self.dropout = nn.Dropout(0.5)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self,x):
         x = self.conv(x)
@@ -65,37 +65,44 @@ class DenseLayer(nn.Module):
     def __init__(self,in_channels,out_channels):
         super().__init__()
         self.dense = nn.Linear(in_channels,out_channels)
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self,x):
         x = self.dense(x)
-        x = self.sigmoid(x)
         return x 
     
-class SpikeNet(nn.Module):
-    def __init__(self,n_eeg_channels):
+class SpikeNetHead(nn.Module):
+    def __init__(self,in_channels,out_channels):
         super().__init__()
-        block1 = FirstConvBlock(in_channels=1,out_channels=32)
-        block2 = SecondConvBlock(in_channels=32,out_channels=64,n_eeg_channels=n_eeg_channels)
-        block3 = OtherConvBlock(in_channels=64,out_channels=64,downsample=True)
-        block4 = OtherConvBlock(in_channels=64,out_channels=94,downsample=False)
-        block5 = OtherConvBlock(in_channels=94,out_channels=94,downsample=True)
-        block6 = OtherConvBlock(in_channels=94,out_channels=128,downsample=False)
-        self.blocks = [block1,block2,block3,block4,block5,block6]
-        self.dense = DenseLayer(128*2,1)
+        self.dense = DenseLayer(in_channels,out_channels)
+        self.sigmoid = torch.sigmoid
+
+    def forward(self,x):
+        x = x.reshape(x.shape[0],-1)
+        x = self.dense(x)
+        x = torch.sigmoid(x)
+        return x
+    
+class SpikeNet(nn.Module):
+    def __init__(self,n_eeg_channels,dropout=0.2):
+        super().__init__()
+        self.block1 = FirstConvBlock(in_channels=1,out_channels=32,dropout=dropout)
+        self.block2 = SecondConvBlock(in_channels=32,out_channels=64,n_eeg_channels=n_eeg_channels,dropout=dropout)
+        self.block3 = OtherConvBlock(in_channels=64,out_channels=64,dropout=dropout,downsample=True)
+        self.block4 = OtherConvBlock(in_channels=64,out_channels=94,dropout=dropout,downsample=False)
+        self.block5 = OtherConvBlock(in_channels=94,out_channels=94,dropout=dropout,downsample=True)
+        self.block6 = OtherConvBlock(in_channels=94,out_channels=128,dropout=dropout,downsample=False)
+        
 
     def forward(self,x):
         # input shape [batch_size, eeg_channels, timesteps]
         # bring into [batch_size, channels,timesteps,eeg_channels]
         x = x.permute(0,2,1)
         x = x.unsqueeze(1)
-        for i, block in enumerate(self.blocks):
-            x = block(x)
-            print(f'block_{i+1}', x.shape)
-        x = x.reshape(x.shape[0],-1)
-        x = self.dense(x)
+        
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
         return x
-
-x = torch.ones(12,18,128)
-model = SpikeNet(n_eeg_channels=18)
-model(x).shape
